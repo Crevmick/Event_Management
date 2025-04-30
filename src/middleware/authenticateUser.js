917556611b444e6a730e3c426a3eba0dfc38bb9b
crevmick <aworetanbidemi@gmail.com>
2025-04-30 15:41:06 +0100

    Fix: fix header issue

--- src/middleware/authenticateUser.js
+++ src/middleware/authenticateUser.js
@@ -9,13 +9,18 @@ const authenticateUser = (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.TOKEN_KEY);
         req.user = {
-            userId: decoded.userId  || user._id,  
-            role: decoded.role, 
-          };
-      
-        next();
+            userId: decoded.userId || user._id,
+            role: decoded.role,
+        };
+        
+        // Ensure that a response is not already sent before calling next
+        if (!res.headersSent) {
+            return next();
+        }
     } catch (error) {
-        res.status(401).json({ error: 'Invalid token' });
+        if (!res.headersSent) {
+            return res.status(401).json({ error: 'Invalid token' });
+        }
     }
 }
 
